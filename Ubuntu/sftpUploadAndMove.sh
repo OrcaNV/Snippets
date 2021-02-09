bb100cdd384353df39ea3912ec80a5e47d7141b7
PreskoFan <preskofan@sensortransport.com>
2021-02-09 14:52:13 -0800

    Update sftpUploadAndMove.sh

--- Ubuntu/sftpUploadAndMove.sh
+++ Ubuntu/sftpUploadAndMove.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+TEMP_SCRIPT=TEMP_STATUS_SCRIPT.sh
 SRC_DIR=/ftp/outbound/
 DONE_DIR=/ftp/outbound/sent
 
 pushAndMove() {
   ediFile=$1
   echo Pushing and Moving $ediFile
-  echo 'sftp -i /ftp/privateKey.ppk sftpuser@sftpServer <<EOF' > TEMP_SCRIPT.sh
-  echo 'cd ftp_in' >> TEMP_SCRIPT.sh
-  echo 'put ' $ediFile >> TEMP_SCRIPT.sh
-  echo 'quit' >>  TEMP_SCRIPT.sh
-  echo ' ' >>  TEMP_SCRIPT.sh
-  echo 'EOF' >>  TEMP_SCRIPT.sh
-  # cat TEMP_SCRIPT.sh
-  chmod +x TEMP_SCRIPT.sh
-  /bin/bash TEMP_SCRIPT.sh
+  echo 'sftp -i /ftp/privateKey.ppk sftpuser@sftpServer <<EOF' > $TEMP_SCRIPT
+  echo 'cd ftp_in' >> $TEMP_SCRIPT
+  echo 'put ' $ediFile >> $TEMP_SCRIPT
+  echo 'quit' >>  $TEMP_SCRIPT
+  echo ' ' >>  $TEMP_SCRIPT
+  echo 'EOF' >>  $TEMP_SCRIPT
+  # cat $TEMP_SCRIPT
+  chmod +x $TEMP_SCRIPT
+  /bin/bash $TEMP_SCRIPT
   mv $ediFile $DONE_DIR
-  echo '*************************************'
-  echo ' '
-  echo ' '
 }
 
-
+echo ' '
+echo ' '
+date
+echo '************************************* Starting'
 cd $SRC_DIR
 for filename in $SRC_DIR/*.xml; do
   pushAndMove ${filename##*/}
+  date
 done
+
+date
+echo 'Done'
